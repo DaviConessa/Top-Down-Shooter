d2709b05fb3018670157164efcbfc38e9db90ccb
DaviConessa <conessadavi.artium@gmail.com>
2025-10-28 22:31:01 -0300

    chore : Added Playercontroler

--- Assets/Project/Scripts/input/PlayerControler.cs
+++ Assets/Project/Scripts/input/PlayerControler.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
-using UnityEngine.inputsystem;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal.Internal;
 
-public class PlayerControler: MonoBehaviour
-
+public class PlayerControler : MonoBehaviour
 {
-    
+    private PlayerInput _inputs;
+    private InputAction _action;
+
+    void Awake()
+    {
+        _inputs = GetComponent<PlayerInput>();
+        _action = _inputs.actions["Move"];
+        _action.ReadValue<Vector2>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 inputMove = _action.ReadValue<Vector2>();
+    }
 }
